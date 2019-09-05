1d874c41a3d804af89a08528a84ba039a5efc1a6
bridget cromien <bcromien@gmail.com>
2019-09-05 18:58:37 -0500

    Completed Techjobs MVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -3,7 +3,9 @@ package org.launchcode.controllers;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,6 +24,20 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="results")
+    public String searchThisBloke(Model model,
+                                  @RequestParam String searchTerm,
+                                  @RequestParam String searchType) {
+        ArrayList<HashMap<String, String>> filteredJobs;
+
+        if (searchType.equals("all")) {
+            filteredJobs = JobData.findByValue(searchTerm);
+        } else {
+            filteredJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobPosts", filteredJobs);
+        return "search";
+    }
 
 }
